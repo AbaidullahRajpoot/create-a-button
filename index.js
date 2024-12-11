4fea02286569bd593458a56b8ec3fd24cf8d59ae
Abaidullah <abaidullah.gwl@gmail.com>
2024-12-11 15:41:37 +0500

    add

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+}));
 
 // Connect to database
 ConnectDb();
